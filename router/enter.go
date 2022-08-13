c047699b191bfbf5b5272cabda7651bc15f2cb34
miaojiaxi <3320795610@qq.com>
2022-08-13 11:09:43 +0800

    first commit

---
+++ router/enter.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/miaojiaxi2004/go_server/router/example"
+	"github.com/miaojiaxi2004/go_server/router/system"
+)
+
+type RouterGroup struct {
+	System  system.RouterGroup
+	Example example.RouterGroup
+}
+
+//
+var RouterGroupApp = new(RouterGroup)
